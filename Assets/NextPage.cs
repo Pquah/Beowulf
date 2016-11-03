d26fb2d94eadbd0668e4d25fda84d6916592ac3c
Pquah <Patrick.s.quah@gmail.com>
2016-11-03 05:26:57 -0700

    drawing pages implemented

---
+++ Assets/NextPage.cs
@@ -0,0 +1,35 @@
+﻿//using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+//public class NextPage : MonoBehaviour {
+//	void Start () {
+//		// Only specifying the sceneName or sceneBuildIndex will load the scene with the Single mode
+
+//	}
+//
+//	void OnMouseUpAsButton() {
+//		SceneManager.LoadScene (2, LoadSceneMode.Additive);
+//	}
+//}
+
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextPage : MonoBehaviour {
+	public Button NextPageButton;
+
+	void Start () {
+		Button btn = NextPageButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		Debug.Log ("You have clicked the button!");
+		//SceneManager.LoadScene (2, LoadSceneMode.Additive);
+
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
